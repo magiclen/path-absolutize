e95b613167398297b119f3c63d68f09550b3cfa2
Magic Len <len@magiclen.org>
2022-04-07 23:32:28 +0800

    improve code

--- src/windows.rs
+++ src/windows.rs
@@ -34,14 +34,8 @@ impl Absolutize for Path {
                             Component::CurDir => {
                                 // may be unreachable
 
-                                let mut cwd_iter = cwd.iter().skip(1);
-
-                                if let Some(token) = cwd_iter.next() {
+                                for token in cwd.iter().skip(1) {
                                     tokens.push(token);
-
-                                    for token in cwd_iter {
-                                        tokens.push(token);
-                                    }
                                 }
 
                                 has_change = true;
@@ -49,14 +43,8 @@ impl Absolutize for Path {
                             Component::ParentDir => {
                                 match cwd.parent() {
                                     Some(cwd_parent) => {
-                                        let mut cwd_parent_iter = cwd_parent.iter().skip(1);
-
-                                        if let Some(token) = cwd_parent_iter.next() {
+                                        for token in cwd_parent.iter().skip(1) {
                                             tokens.push(token);
-
-                                            for token in cwd_parent_iter {
-                                                tokens.push(token);
-                                            }
                                         }
                                     }
                                     None => {
@@ -67,14 +55,8 @@ impl Absolutize for Path {
                                 has_change = true;
                             }
                             _ => {
-                                let mut cwd_iter = cwd.iter().skip(1);
-
-                                if let Some(token) = cwd_iter.next() {
+                                for token in cwd.iter().skip(1) {
                                     tokens.push(token);
-
-                                    for token in cwd_iter {
-                                        tokens.push(token);
-                                    }
                                 }
 
                                 tokens.push(second_component.as_os_str());
